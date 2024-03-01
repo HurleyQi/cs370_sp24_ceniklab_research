93f2558c978ff1a2c53ae8970a8528f1dcd6a0ce
Hurley Qi <qikangsheng@gmail.com>
2024-02-29 22:29:18 -0600

    fastqc:false try

--- riboflow-0.0.0/work/4b/539d89268f1f244cedf7f07587d1cb/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-ribopy create -n GSM1169543 	             --reference appris-v1 	             --lengths appris_celegans_v1_transcript_lengths_new.tsv 							 --annotation appris_celegans_v1_actual_regions_new.bed 							 --radius 50 							 -l 35 -r 10 							 --lengthmin 21 							 --lengthmax 40                --expmeta input.2                --ribometa project.yaml 							  							 -n 1                --alignmentfile GSM1169543.merged.pre_dedup.bed                 GSM1169543.ribo
--- riboflow-0.0.0/work/9c/7285bacdd969ee252479491a27be76/.command.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -ue
-cat correspondence.txt > index_fastq_correspondence.txt
